b207f7f746a7a0db42f93e4e6d410ce798ce53e5
Chris Kairalla <chris@mailtochris.com>
2015-03-14 16:03:43 -0400

    added look sensor prefab and "face object" script so targets will orient to camera

---
+++ Assets/Scripts/FaceObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceObject: MonoBehaviour {
+	public Transform targetObj;
+	//set this to true if you need to orient in the opposite dir.
+	public bool flipY = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flipY){
+			transform.rotation = Quaternion.LookRotation(transform.position - targetObj.transform.position);
+		} else {
+			transform.LookAt(targetObj);
+		}
+	}
+}
